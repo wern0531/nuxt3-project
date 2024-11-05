738862196d15e8329239795e494c362796b6fba9
wern0531 <wern780531@hotmail.com>
2024-11-05 22:53:03 +0800

    create

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <h1>Page: index</h1>
+  <p>目前路由的路徑 - route.fullPath 為 : {{ route.fullPath }}</p>
+</template>
+
+<style scoped></style>
