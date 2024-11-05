824a8ffaae72c8a0c007c5832f98ca4671b66ca1
wern0531 <wern780531@hotmail.com>
2024-11-05 23:32:33 +0800

    add css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: ["@/assets/stylesheets/all.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+           @import "bootstrap/scss/functions";
+           @import "bootstrap/scss/variables"; 
+          `,
+        },
+      },
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,20 @@ const route = useRoute();
 
 <template>
   <h1>Page: index</h1>
-  <p>目前路由的路徑 - route.fullPath 為 : {{ route.fullPath }}</p>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+h1 {
+  color: $primary;
+}
+</style>
